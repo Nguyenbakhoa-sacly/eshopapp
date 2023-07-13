8dbf578f33f49439c7bef8901bb581d3c0ff02b4
Nguyenbahoa-sacly <khoab2306@gmail.com>
2023-07-13 23:26:23 +0700

    headerMobile&footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,17 @@
 
 
+import styles from './Footer.module.scss'
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles)
+
+
+const date = new Date()
+const year = date.getFullYear()
 function Footer() {
     return ( 
-        <div>footer</div>
+        <div className={cx('footer')}>
+            &copy; {year} All Rights Reserved
+        </div>
     );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,9 @@
 
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 import { HiOutlineMenuAlt3 } from 'react-icons/hi'
-import { Link } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa'
+import { Link, NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
 
@@ -28,30 +30,90 @@ const cart = (
     </span>
 )
 
+const activeLInk = ({isActive})=>(
+    isActive ? `${cx('active')}` :''
+)
 
 function Header() {
+
+    const [ showMenu, setShowMenu ] = useState(false)
+
+    const taggleMenu = ()=>{
+        setShowMenu( !showMenu );
+    }
+    const  hideMenu = ()=>{
+        setShowMenu( false );
+    }
     return ( 
         <header>
             <div className={cx('header')}>
                 {logo}
-                <nav>
-                    <ul>
-                        <li><Link to='/'> Home </Link></li>
-                        <li><Link to='/Contact'> Contact Us </Link></li>
+                <nav className={ showMenu ? `${ cx('show-nav')}`: `${ cx('hide-menu')}` }>
+
+                    <div 
+                        onClick={hideMenu} 
+                        className={ 
+                            showMenu ? `${cx('nav-wrapper')} ${cx('show-nav-wrapper')}` : 
+                            `${cx('nav-wrapper')}` 
+                        }>
+                    </div>
+
+                    <ul onClick={ hideMenu }>
+                        <li className={cx('logo-mobile')}>
+                            {logo}
+                            <FaTimes 
+                                size={22} 
+                                color='#fff'
+                                onClick={ hideMenu }
+                            />
+                        </li>
+                        <li>
+                            <NavLink 
+                                to='/' 
+                                className={ activeLInk }> 
+                                Home 
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                to='/Contact'   
+                                className={ activeLInk }
+                            > Contact Us 
+                            </NavLink>
+                        </li>
                     </ul>
-                    <div className={cx('header-right')}>
+                    <div 
+                        className={cx('header-right')}
+                        onClick={ hideMenu }
+                    >
                         <span className={cx('links')}>
-                            <Link to='/login'> Login </Link>
-                            <Link to='/register'> Register </Link>
-                            <Link to='/order-history'> My orders </Link>
+                            <NavLink 
+                                className={activeLInk}
+                                to='/login'
+                            > 
+                                Login 
+                            </NavLink>
+                            <NavLink 
+                                className={activeLInk}
+                                to='/register'
+                            > Register 
+                            </NavLink>
+                            <NavLink 
+                                className={activeLInk}
+                                to='/order-history'
+                            > My orders 
+                            </NavLink>
                         </span>
                         {cart}
                     </div>
+                    
                 </nav>
 
                 <div className={cx('menu-icon')}>
                     {cart}
-                    < HiOutlineMenuAlt3  size={28}/>
+                    < HiOutlineMenuAlt3  size={28}
+                    onClick={ taggleMenu }
+                    />
                 </div>
             </div>
 
