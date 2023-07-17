a04b0fcae9d6b647403534c7fb685a26ee7ca873
Nguyenbahoa-sacly <khoab2306@gmail.com>
2023-07-18 00:51:51 +0700

    reset-pass

--- src/pages/auth/Reset.js
+++ src/pages/auth/Reset.js
@@ -1,12 +1,33 @@
 
 
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import { sendPasswordResetEmail } from 'firebase/auth'
+import { toast } from 'react-toastify';
+import { auth } from '../../firebase/config';
 import { Card } from '../../components/index'
 import imgReset from '../../assets/img/reset.png'
 import classNames from "classnames/bind";
 import styles from './Auth.module.scss';
 const cx = classNames.bind(styles)
 function Reset() {
+
+
+    const [ email, setEmail] = useState('')
+
+    const resetPassword =(e)=>{
+        e.preventDefault();
+
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            toast.success(' Check your email for a reset link')
+        })
+        .catch((error) => {
+            toast.error(error.message)
+            
+        });
+
+    }
     return ( 
         
         <section className={cx('auth', 'container')}>
@@ -20,10 +41,17 @@ function Reset() {
             <Card>
                 <div className={cx('form')}>
                     <h2>Reset password</h2>
-
-                    <form>
-                        <input type="text" placeholder='Email' required/>
-                        <button className="--btn --btn-primary --btn-block" type="submit">Reset password</button>
+                    <form onSubmit={resetPassword}>
+                        <input 
+                            type="text" 
+                            placeholder='Email' required
+                            value={email}
+                            onChange={(e)=> setEmail(e.target.value)}
+                        />
+                        <button 
+                            className="--btn --btn-primary --btn-block" 
+                            type="submit">Reset password
+                        </button>
                         <div className={cx('links')}>
                             <p>
                                 <Link to='/login'>
